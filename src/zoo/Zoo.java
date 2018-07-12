78f97b703d213b1401e437e14cbc9550bede4752
splo2t <yu990410@naver.com>
2018-07-12 18:27:30 +0900

    complete

---
+++ src/zoo/Zoo.java
@@ -0,0 +1,46 @@
+package zoo;
+public class Zoo {
+
+
+    
+    
+    public static void main(String[] args){
+        Master king =new Master();
+        Tiger tigerKing = new Tiger(1);
+        Tiger tigerB=new Tiger();
+        Lion lionA=new Lion();
+        Lion lionB=new Lion();
+        Lion lionKing=new Lion(1);
+        Rabbit rabbit[]=new Rabbit[15];
+        for(int i=0; i<15; i++) {
+            rabbit[i]=new Rabbit();
+        }
+     
+     //tigerFeed-eat
+        king.tigerFeed(tigerKing);
+        king.tigerFeed(tigerKing);
+        king.tigerFeed(tigerKing);
+     
+        king.tigerFeed(tigerB);
+        king.tigerFeed(tigerB);
+        king.tigerFeed(tigerB);
+     
+        king.lionFeed(lionKing);
+        king.lionFeed(lionKing);
+     
+     king.lionFeed(lionA);
+     king.lionFeed(lionA);
+     
+     king.lionFeed(lionB);
+     king.lionFeed(lionB);
+     
+     
+     //lion vs tiger
+      tigerKing.fight(lionKing);
+      tigerKing.fight(lionA);
+      lionB.fight(tigerKing);
+      tigerB.fight(lionB);
+     
+     
+    }
+}
